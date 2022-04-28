ce2e8b70f014b5b61de830fbd5069057dcfce089
tomasz8w <td.wieclaw@gmail.com>
2022-04-28 14:04:51 +0200

    create project: cra, typescript, mui, react-router

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from "@mui/material";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Box>
+        <Typography variant="h3">Dashboard app</Typography>
+      </Box>
+    </Router>
+  );
+}
+
+export default App;
