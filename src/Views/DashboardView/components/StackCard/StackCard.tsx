21cd66ffcd7aea5dd2194ad5563f87171be32cb6
tomasz8w <td.wieclaw@gmail.com>
2022-04-28 15:55:39 +0200

    StackCard styling

--- src/Views/DashboardView/components/StackCard/StackCard.tsx
+++ src/Views/DashboardView/components/StackCard/StackCard.tsx
@@ -1,9 +1,24 @@
-import { Paper, Typography } from "@mui/material";
+import { Box, IconButton, Paper, Typography } from "@mui/material";
+import { MoreVert as MoreIcon } from "@mui/icons-material";
 
 const StackCard = () => {
   return (
-    <Paper elevation={2} sx={{ display: "flex", p: 1 }}>
-      <Typography>stack card</Typography>
+    <Paper
+      elevation={2}
+      sx={{
+        display: "flex",
+        p: 1,
+        flex: "auto",
+        maxWidth: "300px",
+        justifyContent: "center",
+      }}
+    >
+      <Box sx={{ display: "flex", flexDirection: "row", alignItems: "center" }}>
+        <Typography>Stack card</Typography>
+        <IconButton>
+          <MoreIcon />
+        </IconButton>
+      </Box>
     </Paper>
   );
 };
